be967a81f7d1afc7c6587c024bbb9fbfcc1d6f1a
TelesphoreUwabera <t.uwabera@alustudent.com>
2025-01-22 13:03:22 +0200

    :

--- React_component/README.md
+++ React_component/README.md
@@ -1,1 +1,1 @@
-React Component
+# React_component
--- React_component/task_0/dashboard/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders welcome message', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Welcome to My React App/i); // Change the text here
-  expect(linkElement).toBeInTheDocument();
-});
